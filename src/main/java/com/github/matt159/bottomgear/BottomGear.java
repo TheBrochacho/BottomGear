936d143fd121189896e33a272eed0424520d3804
matt-159 <16054364+matt-159@users.noreply.github.com>
2021-10-25 23:08:33 -0700

    changing fields from template defaults

--- src/main/java/com/github/matt159/bottomgear/BottomGear.java
+++ src/main/java/com/github/matt159/bottomgear/BottomGear.java
@@ -1,12 +1,12 @@
-package com.github.basdxz.examplemod;
+package com.github.matt159.bottomgear;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod {
+@Mod(modid = BottomGear.MODID, name = BottomGear.NAME, version = BottomGear.VERSION)
+public class BottomGear {
     public static final String MODID = "examplemod";
     public static final String NAME = "Example Mod";
     public static final String VERSION = "@version@";
