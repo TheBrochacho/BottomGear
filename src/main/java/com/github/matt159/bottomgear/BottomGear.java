8087f004783345a4e7348159e20203e84a919036
matt-159 <16054364+matt-159@users.noreply.github.com>
2021-11-07 01:55:24 -0800

    Added startup check for Baubles and Traveller's Gear

--- src/main/java/com/github/matt159/bottomgear/BottomGear.java
+++ src/main/java/com/github/matt159/bottomgear/BottomGear.java
@@ -3,6 +3,8 @@ package com.github.matt159.bottomgear;
 import com.github.matt159.bottomgear.events.MobSpawnListener;
 import com.github.matt159.bottomgear.events.PlayerListener;
 import com.github.matt159.bottomgear.item.BottomStick;
+import com.github.matt159.bottomgear.util.BGConfig;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -10,7 +12,10 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 
-@Mod(modid = BottomGear.MODID, name = BottomGear.NAME, version = BottomGear.VERSION, dependencies = "required-after:NotEnoughItems")
+@Mod(modid = BottomGear.MODID, name = BottomGear.NAME, version = BottomGear.VERSION,
+        dependencies = "after:Baubles; "
+        + "after:TravellersGear; "
+    )
 public class BottomGear {
     public static final String MODID = "bottomgear";
     public static final String NAME = "Bottom Gear";
@@ -20,6 +25,10 @@ public class BottomGear {
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
+
+        BGConfig.isBaublesLoaded = Loader.isModLoaded("Baubles");
+        BGConfig.isTravellersGearLoaded = Loader.isModLoaded("TravellersGear");
+
         MinecraftForge.EVENT_BUS.register(new PlayerListener());
         MinecraftForge.EVENT_BUS.register(new MobSpawnListener());
 
