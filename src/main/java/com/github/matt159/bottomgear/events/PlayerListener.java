83f7558e12328a8cc76e90bbc70ba823c2046fb2
matt-159 <16054364+matt-159@users.noreply.github.com>
2021-11-07 00:02:43 -0700

    now using the onPlayerTick event instead

--- src/main/java/com/github/matt159/bottomgear/events/PlayerListener.java
+++ src/main/java/com/github/matt159/bottomgear/events/PlayerListener.java
@@ -2,16 +2,9 @@ package com.github.matt159.bottomgear.events;
 
 import com.github.matt159.bottomgear.data.PlayerGearScores;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.PlayerEvent;
-import cpw.mods.fml.common.gameevent.TickEvent.PlayerTickEvent;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemTool;
-import net.minecraft.util.ChatComponentText;
-import net.minecraftforge.client.event.GuiOpenEvent;
-import net.minecraftforge.event.entity.player.PlayerOpenContainerEvent;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
 
 public class PlayerListener {
@@ -23,30 +16,25 @@ public class PlayerListener {
     }
 
     @SubscribeEvent
-    public void onPlayerTick(PlayerTickEvent event) {
-        System.out.println("lmao");
-    }
+    public void onPlayerTick(LivingUpdateEvent event) {
+        if (event.entity instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) event.entity;
+            if (!player.worldObj.isRemote) {
+                int numGearItems = 0;
 
-    @SubscribeEvent
-    public void onPlayerOpenContainer(PlayerOpenContainerEvent event) {
-        if (!event.entityPlayer.worldObj.isRemote) {
-            int numGearItems = 0;
-
-            EntityPlayer player = event.entityPlayer;
-
-            //counting items in the armor slots
-            for (int i = 0; i < 4; i++) {
-                numGearItems += player.getCurrentArmor(i) != null ? 1 : 0;
-            }
-
-            for (int i = 0; i < player.inventory.mainInventory.length; i++) {
-                if (player.inventory.mainInventory[i] != null) {
-                    numGearItems += player.inventory.mainInventory[i].getItem() instanceof ItemTool ? 1 : 0;
+                //counting items in the armor slots
+                for (int i = 0; i < 4; i++) {
+                    numGearItems += player.getCurrentArmor(i) != null ? 1 : 0;
                 }
-            }
 
-//            System.out.println("numGearItems: " + numGearItems);
-            PlayerGearScores.getInstance().put(player.getUniqueID(), numGearItems);
+                for (int i = 0; i < player.inventory.mainInventory.length; i++) {
+                    if (player.inventory.mainInventory[i] != null) {
+                        numGearItems += player.inventory.mainInventory[i].getItem() instanceof ItemTool ? 1 : 0;
+                    }
+                }
+
+                PlayerGearScores.getInstance().put(player.getUniqueID(), numGearItems);
+            }
         }
     }
 }
