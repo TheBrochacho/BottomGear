d257f884017faf93547900c1a0c85e153b296edb
matt-159 <16054364+matt-159@users.noreply.github.com>
2021-11-08 20:41:23 -0800

    Equipment list is built from various player inventories

--- src/main/java/com/github/matt159/bottomgear/events/PlayerListener.java
+++ src/main/java/com/github/matt159/bottomgear/events/PlayerListener.java
@@ -1,11 +1,19 @@
 package com.github.matt159.bottomgear.events;
 
+import baubles.api.IBauble;
+import baubles.common.container.InventoryBaubles;
+import baubles.common.lib.PlayerHandler;
 import com.github.matt159.bottomgear.data.PlayerGearScores;
+import com.github.matt159.bottomgear.util.BGConfig;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemTool;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.*;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
+import tconstruct.armor.player.TPlayerStats;
+
+import java.util.ArrayList;
 
 public class PlayerListener {
 
@@ -17,24 +25,47 @@ public class PlayerListener {
 
     @SubscribeEvent
     public void onPlayerTick(LivingUpdateEvent event) {
-        if (event.entity instanceof EntityPlayer) {
-            EntityPlayer player = (EntityPlayer) event.entity;
-            if (!player.worldObj.isRemote) {
-                int numGearItems = 0;
+        if (!(event.entity instanceof EntityPlayer)) {
+            return;
+        }
 
-                //counting items in the armor slots
-                for (int i = 0; i < 4; i++) {
-                    numGearItems += player.getCurrentArmor(i) != null ? 1 : 0;
+        ArrayList<ItemStack> equipment = new ArrayList<>();
+        EntityPlayer player = (EntityPlayer) event.entity;
+
+        if (!player.worldObj.isRemote) {
+
+            for (ItemStack item : player.inventory.armorInventory) {
+                if (item != null) {
+                    equipment.add(item);
                 }
+            }
 
-                for (int i = 0; i < player.inventory.mainInventory.length; i++) {
-                    if (player.inventory.mainInventory[i] != null) {
-                        numGearItems += player.inventory.mainInventory[i].getItem() instanceof ItemTool ? 1 : 0;
+            for (int i = 0; i < InventoryPlayer.getHotbarSize(); ++i) {
+                ItemStack item = player.inventory.getStackInSlot(i);
+                if (item != null && (item.getItem() instanceof ItemSword || item.getItem() instanceof ItemBow)) {
+                    equipment.add(item);
+                }
+            }
+
+            if (BGConfig.isBaublesLoaded) {
+                InventoryBaubles baubles = PlayerHandler.getPlayerBaubles(player);
+                for (ItemStack item : baubles.stackList) {
+                    if (item != null && item.getItem() instanceof IBauble) {
+                        equipment.add(item);
                     }
                 }
-
-                PlayerGearScores.getInstance().put(player.getUniqueID(), numGearItems);
             }
+
+            if (BGConfig.isTinkersLoaded) {
+                TPlayerStats tps = TPlayerStats.get(player);
+                for (ItemStack item : tps.armor.inventory) {
+                    if (item != null) {
+                        equipment.add(item);
+                    }
+                }
+            }
+
+            PlayerGearScores.getInstance().put(player.getUniqueID(), equipment.size());
         }
     }
 }
