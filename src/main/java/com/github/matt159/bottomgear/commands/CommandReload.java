87681bafc1d8d841684353d9ab39d5120d534ee3
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-06-15 12:57:59 -0700

    Added debug message to console

--- src/main/java/com/github/matt159/bottomgear/commands/CommandReload.java
+++ src/main/java/com/github/matt159/bottomgear/commands/CommandReload.java
@@ -4,6 +4,7 @@ import com.github.matt159.bottomgear.BottomGear;
 import com.github.matt159.bottomgear.util.FileParser;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
+import net.minecraft.util.ChatComponentText;
 
 public class CommandReload extends SubCommand {
 
@@ -19,20 +20,29 @@ public class CommandReload extends SubCommand {
         }
 
         try {
+            StringBuilder debugMessage = new StringBuilder();
+
             switch (args[0]) {
                 case "gear":
                     FileParser.parseGearConfigFile(BottomGear.gearConfigFile);
+                    debugMessage.append("Reloaded gear scores from file.");
                     break;
                 case "dims":
                     FileParser.parseDimConfigFile(BottomGear.dimConfigFile);
+                    debugMessage.append("Reloaded dimension thresholds from file.");
                     break;
                 case "all":
                     FileParser.parseGearConfigFile(BottomGear.gearConfigFile);
                     FileParser.parseDimConfigFile(BottomGear.dimConfigFile);
+                    debugMessage.append("Reloaded gear scores from file.")
+                                .append("\n")
+                                .append("Reloaded dimension thresholds from file.");
                     break;
                 default:
                     throw new WrongUsageException("Options are \"gear\", \"dims\" or \"all\"");
             }
+
+            sender.addChatMessage(new ChatComponentText(debugMessage.toString()));
         } catch (Exception e) {
             e.printStackTrace();
         }
