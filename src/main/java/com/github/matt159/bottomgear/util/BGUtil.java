fb74f524622f75414831e37761eab28814a82f7c
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-06-15 20:22:18 -0700

    moved functions to BGUtil class

---
+++ src/main/java/com/github/matt159/bottomgear/util/BGUtil.java
@@ -0,0 +1,18 @@
+package com.github.matt159.bottomgear.util;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.ItemStack;
+
+public class BGUtil {
+    public static String itemStackToEntry(ItemStack itemStack) {
+        return GameRegistry.findUniqueIdentifierFor(itemStack.getItem()).toString() + "@" + itemStack.getItemDamage();
+    }
+
+    public static ItemStack entryToItemStack(String entry) {
+        String[] itemInfo = entry.split("@");
+        String[] itemName = itemInfo[0].split(":");
+        int damageValue = Integer.parseInt(itemInfo[1]);
+
+        return new ItemStack(GameRegistry.findItem(itemName[0], itemName[1]), 1 , damageValue);
+    }
+}
