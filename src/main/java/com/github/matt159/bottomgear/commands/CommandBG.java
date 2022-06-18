1b909911a6f1eaa993f74e4226928984c7f1adc8
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-06-18 13:58:06 -0700

    added printHelp() function

--- src/main/java/com/github/matt159/bottomgear/commands/CommandBG.java
+++ src/main/java/com/github/matt159/bottomgear/commands/CommandBG.java
@@ -4,6 +4,8 @@ import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
 
 import java.util.*;
 
@@ -38,27 +40,39 @@ public class CommandBG extends SubCommand {
             throw new WrongUsageException("Too few arguments");
         }
 
-        switch (args[0]) {
-            case "help":
-                sender.addChatMessage(new ChatComponentText("Use \"/bottomgear reload\" to reload gear/dimension values from file"));
-                break;
-            default:
-                if (!children.containsKey(args[0])) {
-                    throw new WrongUsageException("Invalid Arguments. Run \"/bottomgear help\" to see a list of commands.");
-                }
+        if ("help".equals(args[0])) {
+            printHelp(sender, this);
+        } else {
+            if (!children.containsKey(args[0])) {
+                throw new WrongUsageException("Invalid Arguments. Run \"/bottomgear help\" to see a list of commands.");
+            }
 
-                ICommand command = children.get(args[0]);
-                if (command.canCommandSenderUseCommand(sender)) {
-                    String[] newArgs = new String[args.length - 1];
-                    System.arraycopy(args, 1, newArgs, 0, newArgs.length);
-                    command.processCommand(sender, newArgs);
-                }
-                break;
+            ICommand command = children.get(args[0]);
+            if (command.canCommandSenderUseCommand(sender)) {
+                String[] newArgs = new String[args.length - 1];
+                System.arraycopy(args, 1, newArgs, 0, newArgs.length);
+                command.processCommand(sender, newArgs);
+            }
         }
     }
 
-    @Override
-    public int getRequiredPermissionLevel() {
-        return 0;
+    @SuppressWarnings("unchecked")
+    public static void printHelp(ICommandSender sender, SubCommand command) {
+        ChatStyle header = new ChatStyle().setColor(EnumChatFormatting.AQUA);
+        sender.addChatMessage(new ChatComponentText("Format: /bottomgear <sub-command>").setChatStyle(header));
+
+        List<String> text = new LinkedList<>();
+        ChatStyle body = new ChatStyle().setColor(EnumChatFormatting.GRAY);
+
+        text.add("Aliases: " + String.join(", ", command.getCommandAliases()));
+        text.add("Permission Level: " + command.permLevel.permLevel);
+        text.add("Various Sub-Commands for Bottom Gear");
+        text.forEach(line -> sender.addChatMessage(new ChatComponentText(line).setChatStyle(body)));
+
+        text.clear();
+        ChatStyle footer = new ChatStyle().setColor(EnumChatFormatting.WHITE);
+        text.add("Available Sub-Commands");
+        command.getChildren().values().forEach(child -> text.add("- " + child.getCommandName()));
+        text.forEach(line -> sender.addChatMessage(new ChatComponentText(line).setChatStyle(footer)));
     }
 }
