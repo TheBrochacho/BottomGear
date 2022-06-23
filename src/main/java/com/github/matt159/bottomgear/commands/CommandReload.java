1e81ee0eec8d2b1c255999610e87b0970228144f
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-06-23 16:32:34 -0700

    separated stringbuilder into individual chat messages

--- src/main/java/com/github/matt159/bottomgear/commands/CommandReload.java
+++ src/main/java/com/github/matt159/bottomgear/commands/CommandReload.java
@@ -1,6 +1,5 @@
 package com.github.matt159.bottomgear.commands;
 
-import com.github.matt159.bottomgear.BottomGear;
 import com.github.matt159.bottomgear.util.FileUtil;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
@@ -20,29 +19,25 @@ public class CommandReload extends SubCommand {
         }
 
         try {
-            StringBuilder debugMessage = new StringBuilder();
-
             switch (args[0]) {
                 case "gear":
-                    FileUtil.parseGearConfigFile(BottomGear.gearConfigFile);
-                    debugMessage.append("Reloaded gear scores from file.");
+                    FileUtil.parseGearConfigFile();
+                    sender.addChatMessage(new ChatComponentText("Reloaded gear scores from file."));
                     break;
                 case "dims":
-                    FileUtil.parseDimConfigFile(BottomGear.dimConfigFile);
-                    debugMessage.append("Reloaded dimension thresholds from file.");
+                    FileUtil.parseDimConfigFile();
+                    sender.addChatMessage(new ChatComponentText("Reloaded dimension thresholds from file."));
                     break;
                 case "all":
-                    FileUtil.parseGearConfigFile(BottomGear.gearConfigFile);
-                    FileUtil.parseDimConfigFile(BottomGear.dimConfigFile);
-                    debugMessage.append("Reloaded gear scores from file.")
-                                .append("\n")
-                                .append("Reloaded dimension thresholds from file.");
+                    FileUtil.parseGearConfigFile();
+                    sender.addChatMessage(new ChatComponentText("Reloaded gear scores from file."));
+
+                    FileUtil.parseDimConfigFile();
+                    sender.addChatMessage(new ChatComponentText("Reloaded dimension thresholds from file."));
                     break;
                 default:
                     throw new WrongUsageException("Options are \"gear\", \"dims\" or \"all\"");
             }
-
-            sender.addChatMessage(new ChatComponentText(debugMessage.toString()));
         } catch (Exception e) {
             e.printStackTrace();
         }
